f43fbaeb60038e7ed15aeee39ca52cc6358e1b5d
Mark Peneder <Student@students-macbook-pro.local>
2018-01-12 00:52:03 +0100

    Initial Commit

---
+++ BA2_intuitive_ui_for_complex_rules/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BA2_intuitive_ui_for_complex_rules
+//
+//  Created by Student on 12.01.18.
+//  Copyright © 2018 Peneder. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
